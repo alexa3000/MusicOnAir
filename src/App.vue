6a1e60e333b9c24b598ca5ecb55df6941a7bad71
Alex <alexasahis@gmail.com>
2022-09-16 11:29:20 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="less">
+* {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  padding: 0;
+  margin: 0;
+  color: #fff;
+  background: transparent;
+  text-decoration: none;
+  list-style: none;
+  line-height: 1.6;
+  z-index: 0;
+}
+
+#app {
+  min-width: 568px;
+  width: 100%;
+  min-height: 100vh;
+  position: relative;
+  background-color: rgba(0, 0, 0, 1);
+}
+</style>
